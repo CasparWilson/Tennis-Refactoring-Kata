22c888e7bbdd230f5206b9565a3aafc1f9d40374
CasparWilson <casparwilson111@gmail.com>
2024-08-08 15:07:03 +0100

    changed line 16 to check against the objects player1name property, not an arbitrary string. Will make more robust as game can handle different names as parameters when starting game.

--- java/src/main/java/TennisGame6.java
+++ java/src/main/java/TennisGame6.java
@@ -7,11 +7,13 @@ public class TennisGame6 implements TennisGame {
     public TennisGame6(String player1Name, String player2Name) {
         this.player1Name = player1Name;
         this.player2Name = player2Name;
+        this.player1Score = 0;
+        this.player2Score = 0;
     }
 
     @Override
     public void wonPoint(String playerName) {
-        if (playerName.equals("player1"))
+        if (playerName.equals(this.player1Name))
             player1Score++;
         else
             player2Score++;
